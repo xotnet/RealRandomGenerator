c4bd64e0bd764da642cf1b973f00f2fc5e42e428
xotnet <150250186+xotnet@users.noreply.github.com>
2024-03-09 17:23:00 +0000

    Update realRandomGenerator.hpp

--- realRandomGenerator.hpp
+++ realRandomGenerator.hpp
@@ -1,24 +1,25 @@
 // Real random C++ generator for windows and linux
 #include <iostream>
 #include <ctime>
-long long int generateRealRandomInt(long long int from, long long int upto) { // generateRealRandomInt(minimum, maximum);
-	std::time_t t = std::time(nullptr);
-    std::tm* now = std::localtime(&t);
-	char buffer[128];
-    strftime(buffer, sizeof(buffer), "%m%d%Y%S", now);
-	signed long long int bufToInt = std::stoi(buffer);
-	int gen = 11;
+long long int genFY(long long int from, long long int upto) {
+	int gen = 1;
 	int* getPtr = &gen; // random based on system virtual memory allocate
-	signed long long int generator1 = reinterpret_cast<signed long long int>(getPtr);
-	int agen = generator1 % gen;
-	long long int result = from * agen * generator1 * bufToInt;
-	result = std::abs(result);
-	while (result % 2 == 0 || result % 3 == 0 || result % 5 == 0 || result % 7 == 0) {
-		result++;
+	long long int generator1 = reinterpret_cast<long long int>(getPtr);
+	long long int result = 0;
+	std::string resStr = std::to_string(generator1);
+	char x = resStr[resStr[0]];
+	if (x-'0' < 5 && (x-'0')%2==0) {
+		generator1++;
 	}
-	result = result % upto;
+	if (upto == 1) {result = generator1 % (upto+1);}
+	else {result = generator1 % upto;}
+	return std::abs(result);
+}
+
+long long int generateRealRandomInt(long long int from, long long int upto) { // generateRealRandomInt(minimum, maximum);
+	long long int result = from-1;
 	while (result < from) {
-		result = result * from * generator1 * bufToInt % upto;
+		result = genFY(from,upto);
 	}
 	return std::abs(result);
 }
